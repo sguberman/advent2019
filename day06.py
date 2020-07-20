9b3fe4e323943080f21267814f1cdfaf4cb04c7e
Seth Guberman <sguberman@gmail.com>
2020-07-20 15:36:28 -0400

    complete day06 part2

--- day06.py
+++ day06.py
@@ -1,5 +1,7 @@
-from collections import defaultdict
-from typing import Dict, Iterable, List, Tuple
+from typing import Dict, Iterable, List, Optional, Union
+
+
+ParentDict = Dict[str, str]
 
 
 def parse_line(line: str) -> List[str]:
@@ -16,35 +18,35 @@ def parse_input(inputfile: str) -> Iterable[List[str]]:
     return (parse_line(line) for line in open(inputfile))
 
 
-def build_map(inputfile: str) -> Tuple[Dict[str, str], Dict[str, List[str]]]:
+def build_map(inputfile: str) -> ParentDict:
     '''
     For each orbit in the inputfile, add the relationship to the
-    child->parent and parent->children dictionaries. Return the dictionaries in
-    a tuple.
+    child->parent dictionary. Return the dictionary.
     '''
-    parent_of: Dict[str, str] = {}
-    children_of: Dict[str, List[str]] = defaultdict(list)
+    parent_of: ParentDict = {}
     for parent, child in parse_input(inputfile):
         parent_of[child] = parent
-        children_of[parent].append(child)
-    return parent_of, children_of
+    return parent_of
 
 
-def count_orbits(parent_of: Dict[str, str], child: str) -> int:
+def count_orbits(
+        parent_of: ParentDict,
+        child: str,
+        root: Optional[str] = 'COM') -> int:
     '''
     Count the number of direct and indirect orbits for a given object in space.
     '''
-    if child == 'COM':
+    if child == root:
         return 0
     else:
-        return 1 + count_orbits(parent_of, parent_of[child])
+        return 1 + count_orbits(parent_of, parent_of[child], root)
 
 
 def orbit_checksum(inputfile: str) -> int:
     '''
     Count the total number of direct and indirect orbits in a given inputfile.
     '''
-    parent_of, _ = build_map(inputfile)
+    parent_of = build_map(inputfile)
     return sum(count_orbits(parent_of, child) for child in parent_of)
 
 
@@ -55,5 +57,58 @@ def part1(inputfile: str) -> int:
     return orbit_checksum(inputfile)
 
 
+def path_to_root(
+        parent_of: ParentDict,
+        child: str,
+        root: Optional[str] = 'COM') -> List[str]:
+    '''
+    Trace the path from the child node back to the root of the parent_of dict.
+    Return as a list of str.
+    '''
+    if child == root:
+        return []
+    else:
+        parent = parent_of[child]
+        return [parent] + path_to_root(parent_of, parent, root)
+
+
+def common_node(
+        origin_path: List[str],
+        destination_path: List[str]) -> Union[str, None]:
+    '''
+    Return the first node in origin_path that is also in destination_path.
+    Otherwise return None.
+    '''
+    for node in origin_path:
+        if node in destination_path:
+            return node
+    return None
+
+
+def count_orbital_transfers(
+        parent_of: ParentDict,
+        origin: str,
+        destination: str) -> int:
+    '''
+    Count the minimum number of orbital transfers needed to transfer the origin
+    object to the same orbit as the destination object.
+    '''
+    origin_path = path_to_root(parent_of, origin)
+    destination_path = path_to_root(parent_of, destination)
+    common = common_node(origin_path, destination_path)
+    origin_to_common = count_orbits(parent_of, origin, common)
+    destination_to_common = count_orbits(parent_of, destination, common)
+    return origin_to_common + destination_to_common - 2
+
+
+def part2(inputfile: str) -> int:
+    '''
+    What is the minimum number of orbital transfers required to move the from
+    the object YOU are orbiting to the object SAN is orbiting?
+    '''
+    return count_orbital_transfers(build_map(inputfile), 'YOU', 'SAN')
+
+
 if __name__ == '__main__':
     print(part1('input_day06.txt'))
+    print(part2('input_day06.txt'))
--- test_day06.py
+++ test_day06.py
@@ -7,23 +7,53 @@ def test_parse_line():
 
 
 def test_build_map():
-    parent_of, children_of = day06.build_map('test_input_day06.txt')
+    parent_of = day06.build_map('test_input_day06_part1.txt')
     assert parent_of['H'] == 'G'
-    assert children_of['G'] == ['H']
     assert parent_of['L'] == 'K'
-    assert set(children_of['D']) == set(['E', 'I'])
 
 
 def test_count_orbits():
-    parent_of, _ = day06.build_map('test_input_day06.txt')
+    parent_of = day06.build_map('test_input_day06_part1.txt')
     assert day06.count_orbits(parent_of, 'D') == 3
     assert day06.count_orbits(parent_of, 'L') == 7
     assert day06.count_orbits(parent_of, 'COM') == 0
 
 
 def test_orbit_checksum():
-    assert day06.orbit_checksum('test_input_day06.txt') == 42
+    assert day06.orbit_checksum('test_input_day06_part1.txt') == 42
 
 
 def test_part1():
     assert day06.part1('input_day06.txt') == 241064
+
+
+def test_count_orbital_transfers():
+    parent_of = day06.build_map('test_input_day06_part2.txt')
+    assert day06.count_orbital_transfers(parent_of,
+                                         origin='YOU',
+                                         destination='SAN') == 4
+
+
+def test_path_to_root():
+    parent_of = day06.build_map('test_input_day06_part2.txt')
+    assert day06.path_to_root(parent_of, 'YOU') == [
+        'K', 'J', 'E', 'D', 'C', 'B', 'COM']
+    assert day06.path_to_root(parent_of, 'SAN') == [
+        'I', 'D', 'C', 'B', 'COM']
+
+
+def test_common_node():
+    parent_of = day06.build_map('test_input_day06_part2.txt')
+    origin_path = day06.path_to_root(parent_of, 'YOU')
+    destination_path = day06.path_to_root(parent_of, 'SAN')
+    assert day06.common_node(origin_path, destination_path) == 'D'
+
+
+def test_count_orbits_to_node():
+    parent_of = day06.build_map('test_input_day06_part2.txt')
+    assert day06.count_orbits(parent_of, 'YOU', 'D') == 4
+    assert day06.count_orbits(parent_of, 'SAN', 'D') == 2
+
+
+def test_part2():
+    assert day06.part2('input_day06.txt') == 418
